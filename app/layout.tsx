b0a15aad164b50d8cf491f52cee3bc1f5cf99059
LoongArum <141336798+LoongArum@users.noreply.github.com>
2024-03-10 10:33:52 +0000

    edit titles

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "法学聊天",
+  description: "你占有的法学聊天",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "法学聊天",
     statusBarStyle: "default",
   },
 };
